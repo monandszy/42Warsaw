4ffdb4127a4cd6d5a81d2b22677db6d6df98cbc9
Debian <monandszy@tuta.io>
2025-10-08 16:02:15 +0200

    struct push

---
+++ printf/code/ft_printf.h
@@ -0,0 +1,10 @@
+#ifndef FT_PRINTF
+#	define FT_PRINTF
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdard.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
--- printf/cp_libft.sh
+++ printf/cp_libft.sh
@@ -1,1 +1,1 @@
-cp -r ./../libft/code/ ./code/libft/
+cp -r ./../mylibft/code/ ./code/libft/
