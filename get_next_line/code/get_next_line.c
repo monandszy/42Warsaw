baea688373844a32c33241338a62733a7dc40659
Debian <monandszy@tuta.io>
2025-10-17 10:53:23 +0200

    gnl blueprint

--- get_next_line/code/get_next_line.c
+++ get_next_line/code/get_next_line.c
@@ -15,25 +15,79 @@
 
 char *get_next_line(int fd)
 {
-	char *buf;
-	static int sfd;
-	static char *prev;
+	char **s_chunk;
+	char **chunk;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
+	chunk = (char **) malloc((char *));
+	s_chunk = provide_singleton(fd);
+	if (fd < 0 || BUFFER_SIZE <= 0 || !chunk || !s_chunk)
+		return (free_params(chunk));
+	if (!find_nl(s_chunk, chunk))
+		return (free_params(chunk));
+	return (*s_chunk);
+}
+
+char *free_params(char **chunk)
+{
+	if (chunk)
+	{
+		if (*chunk)
+			free(*chunk);
+		free(chunk);
+	}
+}
+
+int find_nl(char **s_chunk, char **chunk)
+{
+	char *nl;
+	char *join;
+
+	nl = NULL
+	while (!nl)
+	{
+		read_buffer(fd, chunk);
+		nl = strnchr(chunk, '\n');
+		if (!nl)
+			join = strjoin(*s_chunk, *chunk, BUFFER_SIZE);
+		else
+			join = strjoin(*s_chunk, *chunk, nl - *chunk);
+		if !(join)
+			return (NULL);
+		else
+		{
+			free (*s_chunk);
+			*s_chunk = join;
+		}
+	}
+
+	*chunk = *s_chunk;
+	nl++;
+	*s_chunk = nl;
+	return (nl);
+}
+
+void read_buffer(int fd, char **chunk)
+{
+	char *buf;
+
+	buf = (char *) malloc(BUFFER_SIZE + 2);
+	buf[BUFFER_SIZE + 1] = '\0';
+	read(fd, buf, BUFFER_SIZE);
+	*chunk = buf;
+}
+
+char **singleton_manager(int fd)
+{
+	static int sfd;
+	static char **chunk;
+
 	if (sfd != fd || !sfd)
 	{
 		sfd = fd;
-		if (prev)
-			free (prev);
-		prev = NULL;
+		free_params(chunk);
+		chunk = NULL;
 	}
-	printf("[%d]", sfd);
-	printf("[%d]", fd);
-	printf("[%d]", BUFFER_SIZE);
-	buf = (char *) malloc(BUFFER_SIZE);
-	printf("[%zu]", sizeof(buf));
-	read(fd, buf, BUFFER_SIZE - 2);
-	buf[BUFFER_SIZE - 1] = '\0';
-	return (buf);
+	if (!chunk)
+		chunk = (char **) malloc((char *));
+	return (chunk);
 }
