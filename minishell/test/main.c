913cd27e675f7fa3da4556b9698f25c27883f216
Debian <monandszy@tuta.io>
2025-12-14 17:56:07 +0100

    feat: first pipin action

--- minishell/test/main.c
+++ minishell/test/main.c
@@ -9,7 +9,6 @@ void free_cmd(t_cmd *cmd)
   free(cmd); 
 }
 
-
 void test(t_shell *shell, char *in)
 {
   t_cmd *cmd;
@@ -17,10 +16,65 @@ void test(t_shell *shell, char *in)
   errno = 0;
   printf("START: [%s]\n", in);
   cmd = init_single_cmd(shell, in);
+  shell->cmds = cmd;
   execute_cmd_chain(shell, cmd);
   free_cmd(cmd);
   printf("END: [%s]\n", in);
-  // perror("");
+}
+
+void test_single(t_shell *shell)
+{
+  test(shell, "./testexe");
+  test(shell, "/bin/ls");  
+  test(shell, "make");
+  test(shell, "          ");
+  test(shell, "");
+  test(shell, "none");
+  test(shell, "echo -n cat");
+  test(shell, "echo cat");
+
+  test(shell, "env");
+  test(shell, "export CAT=firstcat feaffa");
+  test(shell, "env");
+  test(shell, "export CAT=secondcat");
+  test(shell, "env");
+  test(shell, "unset CAT");
+  test(shell, "env");
+
+  test(shell, "export");
+  test(shell, "unset");
+
+  test(shell, "dir");
+  test(shell, "pwd");
+  test(shell, "cd ..");
+  test(shell, "pwd");
+  test(shell, "cd test");
+  test(shell, "pwd");
+  test(shell, "cd");
+  test(shell, "pwd");
+
+  test(shell, "unset PATH");
+  test(shell, "dir");
+  test(shell, "/bin/ls");
+  test(shell, "export PATH=/bin:/home/user/new/minishell/test");
+  test(shell, "ls");
+}
+
+void test_mutliple(t_shell *shell, char *s1, char *s2, char *s3)
+{
+  errno = 0;
+  t_cmd *c1 = init_single_cmd(shell, s1);
+  t_cmd *c2 = init_single_cmd(shell, s2);
+  t_cmd *c3 = init_single_cmd(shell, s3);
+  c1->next = c2;
+  c2->next = c3;
+  shell->cmds = c1;
+  printf("START: [%s][%s][%s]\n", s1, s2, s3);
+  execute_cmd_chain(shell, c1);
+  free_cmd(c1);
+  free_cmd(c2);
+  free_cmd(c3);
+  printf("END: [%s][%s][%s]\n", s1, s2, s3);
 }
 
 int main(int argc, char **argv, char **envp)
@@ -31,41 +85,9 @@ int main(int argc, char **argv, char **envp)
   (void) argc;
 
   init_shell(&shell, envp);
-  // test(&shell, "./testexe");
-  // test(&shell, "/bin/ls");  
-  // test(&shell, "make");
-  // test(&shell, "          ");
-  // test(&shell, "");
-  // test(&shell, "none");
-  // test(&shell, "echo -n cat");
-  // test(&shell, "echo cat");
-
-
-  // test(&shell, "env");
-  // test(&shell, "export CAT=firstcat feaffa");
-  // test(&shell, "env");
-  // test(&shell, "export CAT=secondcat");
-  // test(&shell, "env");
-  // test(&shell, "unset CAT");
-  // test(&shell, "env");
-
-  // test(&shell, "export");
-  // test(&shell, "unset");
-
-  // test(&shell, "dir");
-  // test(&shell, "pwd");
-  // test(&shell, "cd ..");
-  // test(&shell, "pwd");
-  // test(&shell, "cd test");
-  // test(&shell, "pwd");
-  // test(&shell, "cd");
-  // test(&shell, "pwd");
-
-  // test(&shell, "unset PATH");
-  // test(&shell, "dir");
-  // test(&shell, "/bin/ls");
-  // test(&shell, "export PATH=/bin:/home/user/new/minishell/test");
-  // test(&shell, "ls");
+  // setup_signals();
+  // test_single(&shell);
+  test_mutliple(&shell, "dir", "dir", "dir");
   test(&shell, "exit random arsgument stuff");
   end(&shell, NULL);
 }
