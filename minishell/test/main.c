317a351b5221e7f36e6a4657c8963d34e72a01e9
Debian <monandszy@tuta.io>
2025-12-14 18:25:47 +0100

    feat: pipe tests, pass besides cat | cat | ls

--- minishell/test/main.c
+++ minishell/test/main.c
@@ -60,7 +60,21 @@ void test_single(t_shell *shell)
   test(shell, "ls");
 }
 
-void test_mutliple(t_shell *shell, char *s1, char *s2, char *s3)
+void test_t(t_shell *shell, char *s1, char *s2)
+{
+  errno = 0;
+  t_cmd *c1 = init_single_cmd(shell, s1);
+  t_cmd *c2 = init_single_cmd(shell, s2);
+  c1->next = c2;
+  shell->cmds = c1;
+  printf("START: [%s][%s]\n", s1, s2);
+  execute_cmd_chain(shell, c1);
+  free_cmd(c1);
+  free_cmd(c2);
+  printf("END: [%s][%s]\n", s1, s2);
+}
+
+void test_m(t_shell *shell, char *s1, char *s2, char *s3)
 {
   errno = 0;
   t_cmd *c1 = init_single_cmd(shell, s1);
@@ -77,6 +91,50 @@ void test_mutliple(t_shell *shell, char *s1, char *s2, char *s3)
   printf("END: [%s][%s][%s]\n", s1, s2, s3);
 }
 
+void test_multiple(t_shell *shell)
+{
+  test_m(shell, "dir", "dir", "dir");
+  
+   // ls | grep ".c" | wc -l
+  test_m(shell, "ls", "grep .c", "wc -l");
+  // cat Makefile | head -n 10 | tail -n 1
+  test_m(shell, "cat Makefile", "head -n 10", "tail -n 1");
+  // echo "HelloWorld" | tr "H" "Y" | cat -e
+  test_m(shell, "echo HelloWorld", "tr H Y", "cat -e");
+  // ps aux | grep "usr" | sort -r
+  test_m(shell, "ps aux", "grep usr", "sort -r");
+
+    // echo "1 2 3" | tr ' ' '\n' | grep 2
+  // (Note: C requires double backslash for newline escape)
+  // test_m(shell, "echo 1 2 3", "tr ' ' '\\n'", "grep 2");
+
+  // cat | cat | ls
+  // (Your example: creates a chain where the final command ignores the previous output)
+  // test_m(shell, "cat", "cat", "ls");
+}
+
+void test_two(t_shell *shell)
+{
+  // ls -la | wc -l
+  test_t(shell, "ls -la", "wc -l");
+  // cat /etc/passwd | head -n 5
+  test_t(shell, "cat /etc/passwd", "head -n 5");
+  // echo "hello world" | cat -e
+  test_t(shell, "echo hello world", "cat -e");
+  // who | sort
+  test_t(shell, "who", "sort");
+
+  // yes | head -n 5
+  // (Tests if the pipe closes correctly to stop the infinite 'yes')
+  // test_t(shell, "yes", "head -n 5");
+  // ls | echo "I ignore input"
+  // (Tests if the first process finishes even if the second ignores stdin)
+  test_t(shell, "ls", "echo I ignore input");
+  // sleep 1 | ls
+  // (Tests concurrency/waiting)
+  test_t(shell, "sleep 1", "ls");
+}
+
 int main(int argc, char **argv, char **envp)
 {
   t_shell shell;
@@ -85,9 +143,11 @@ int main(int argc, char **argv, char **envp)
   (void) argc;
 
   init_shell(&shell, envp);
-  // setup_signals();
-  // test_single(&shell);
-  test_mutliple(&shell, "dir", "dir", "dir");
+
+  test_single(&shell);
+  test_two(&shell);
+  test_multiple(&shell);
+ 
   test(&shell, "exit random arsgument stuff");
   end(&shell, NULL);
 }
