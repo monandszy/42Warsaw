88e62ac3b7043d6deca5c2294056aa7b1c664e1b
monandszy <monandszy@tuta.io>
2026-03-03 08:39:26 +0100

    feat: sorted print

--- cpp/cpp09/ex02/pMergeMe.cpp
+++ cpp/cpp09/ex02/pMergeMe.cpp
@@ -11,6 +11,7 @@ Time C2
 // std::list
 // std::vector
 
+#include <algorithm>
 #include <iostream>
 #include <list>
 #include <sstream>
@@ -41,6 +42,29 @@ void initialize(int argc, char* argv[], T& c) {
   }
 }
 
+void print_argv(int argc, char* argv[]) {
+  std::cout << "Unsorted: ";
+  std::cout << "[";
+  for (int i = 1; i < argc; i++) {
+    std::cout << " " << argv[i];
+  }
+  std::cout << " ]" << std::endl;
+}
+
+template <typename T>
+void print_sorted(T& t) {
+  std::cout << "Sorted: ";
+  std::cout << "[";
+  for (typename T::iterator it = t.begin(); it != t.end(); ++it) {
+    std::cout << " " << *it;
+  }
+  std::cout << " ]" << std::endl;
+}
+
+// double sort(int argc, std::vector<int> v) {
+
+// }
+
 int main(int argc, char* argv[]) {
   if (argc == 1) {
     log("ERROR: invalid argument count");
@@ -54,6 +78,12 @@ int main(int argc, char* argv[]) {
   try {
     initialize(argc, argv, v);
     initialize(argc, argv, l);
+    print_argv(argc, argv);
+    std::sort(v.begin(), v.end());
+    l.sort();
+    print_sorted(v);
+    print_sorted(l);
+    // print_sorted(l);
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
