21dd5cf59ea5fab466cd9cc27f9e4415ae3875eb
monandszy <monandszy@tuta.io>
2026-03-03 13:47:28 +0100

    feat: 09 ordering

--- cpp/cpp09/ex02/pMergeMe.cpp
+++ cpp/cpp09/ex02/pMergeMe.cpp
@@ -11,59 +11,104 @@ Time C2
 // std::list
 // std::vector
 
-#include <algorithm>
-#include <iostream>
-#include <list>
-#include <sstream>
-#include <vector>
-
-void log(const std::string& s) { std::cout << "[" << s << "]" << std::endl; }
-
-static bool isnum(const char* str) {
-  double result;
-  std::istringstream i(str);
-  i >> result;
-  return !i.fail() && i.eof();
-}
+#include "pMergeMe.hpp"
 
 template <typename T>
-void initialize(int argc, char* argv[], T& c) {
-  for (int i = 1; i < argc; i++) {
-    if (!isnum(argv[i])) {
-      throw std::runtime_error("ERROR: argument not a number");
-    }
-    int a;
-    std::stringstream si(argv[i]);
-    si >> a;
-    if (a < 0) {
-      throw std::runtime_error("ERROR: argument negative");
-    }
-    c.push_back(a);
+void move(T& it, const T& end, int c) {
+  for (int i = 0; i < c; i++) {
+    if (it == end) break;
+    ++it;
   }
 }
 
-void print_argv(int argc, char* argv[]) {
-  std::cout << "Unsorted: ";
-  std::cout << "[";
-  for (int i = 1; i < argc; i++) {
-    std::cout << " " << argv[i];
-  }
-  std::cout << " ]" << std::endl;
-}
-
-template <typename T>
-void print_sorted(T& t) {
-  std::cout << "Sorted: ";
-  std::cout << "[";
-  for (typename T::iterator it = t.begin(); it != t.end(); ++it) {
-    std::cout << " " << *it;
-  }
-  std::cout << " ]" << std::endl;
-}
-
-// double sort(int argc, std::vector<int> v) {
-
+// if (t.size() == 2) {
+//   typename T::iterator first = t.begin();
+//   typename T::iterator second = first++;
+//   if (*first < *second) {
+//     int tmp;
+//     tmp = *first;
+//     *first = *second;
+//     *second = tmp;
+//   }
+//   return;
 // }
+
+template <typename T>
+void order_pairs(int pow, T& t) {
+  typename T::iterator it = t.begin();
+  typename T::iterator curr_pair_begin = it;
+  typename T::iterator next_pair_begin;
+  move(it, t.end(), pow - 1);
+
+  while (it != t.end()) {
+    typename T::iterator curr = it;
+    move(it, t.end(), 1);
+    next_pair_begin = it;
+    move(it, t.end(), pow - 1);
+    typename T::iterator next = it;
+    if (next != t.end()) {
+      // std::cout << "[" << *curr_pair_begin << "|" << *curr << "|"
+      //           << *next_pair_begin << "|" << *next << "]" << std::endl;
+      if (*curr > *next) {
+        while (1) {
+          int tmp = *curr_pair_begin;
+          *curr_pair_begin = *next_pair_begin;
+          *next_pair_begin = tmp;
+          if (curr_pair_begin == curr) break;
+          ++curr_pair_begin;
+          ++next_pair_begin;
+        }
+      }
+    } else {
+      break;
+    }
+    // print_sorted(t);
+    // std::cout << "a[" << *curr_pair_begin << "|" << *curr << "|"
+    //           << *next_pair_begin << "|" << *next << "]" << std::endl;
+    move(it, t.end(), 1);
+    curr_pair_begin = it;
+    move(it, t.end(), pow - 1);
+  }
+}
+
+// pair as <small, big>
+template <typename T>
+void sort(int argc, int pow, T& t) {
+  if (pow * 2 > argc) return;
+
+  order_pairs(pow, t);
+  sort(argc, pow * 2, t);
+  print_sorted(t);
+
+  T main;
+  T pend;
+
+  typename T::iterator it = t.begin();
+
+  int pairc = (argc / pow);
+  int c = pow * pairc;
+  std::cout << c << "|" << pairc << "|" << pow << std::endl;
+  for (int i = 0; i < c; i++) {
+    if (it == t.end()) break;
+    if (i < pow * 2)
+      main.push_back(*it);
+    else if ((i / pow) % 2 == 0)
+      pend.push_back(*it);
+    else
+      main.push_back(*it);
+    ++it;
+  }
+  std::cout << "main: ";
+  print_sorted(main);
+  std::cout << "pend: ";
+  print_sorted(pend);
+}
+
+// merge insertion sort
+
+// divide into pairs of 2.
+// sort each pair.
+// combine pairs using binary insertion utill all combined
 
 int main(int argc, char* argv[]) {
   if (argc == 1) {
@@ -79,11 +124,12 @@ int main(int argc, char* argv[]) {
     initialize(argc, argv, v);
     initialize(argc, argv, l);
     print_argv(argc, argv);
-    std::sort(v.begin(), v.end());
-    l.sort();
-    print_sorted(v);
+    // sort(v);
+    sort(argc, 1, l);
+    // std::sort(v.begin(), v.end());
+    // l.sort();
+    // print_sorted(v);
     print_sorted(l);
-    // print_sorted(l);
   } catch (std::exception& e) {
     std::cout << e.what() << std::endl;
   }
---
+++ cpp/cpp09/ex02/pMergeMe.hpp
@@ -0,0 +1,55 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <vector>
+
+void log(const std::string& s) { std::cout << "[" << s << "]" << std::endl; }
+
+static bool isnum(const char* str) {
+  double result;
+  std::istringstream i(str);
+  i >> result;
+  return !i.fail() && i.eof();
+}
+
+template <typename T>
+void initialize(int argc, char* argv[], T& c) {
+  for (int i = 1; i < argc; i++) {
+    if (!isnum(argv[i])) {
+      throw std::runtime_error("ERROR: argument not a number");
+    }
+    int a;
+    std::stringstream si(argv[i]);
+    si >> a;
+    if (a < 0) {
+      throw std::runtime_error("ERROR: argument negative");
+    }
+    c.push_back(a);
+  }
+}
+
+void print_argv(int argc, char* argv[]) {
+  std::cout << "Unsorted: ";
+  std::cout << "[";
+  for (int i = 1; i < argc; i++) {
+    std::cout << " " << argv[i];
+  }
+  std::cout << " ]" << std::endl;
+}
+
+template <typename T>
+void print_sorted(T& t) {
+  std::cout << "Sorted: ";
+  std::cout << "[";
+  for (typename T::iterator it = t.begin(); it != t.end(); ++it) {
+    std::cout << " " << *it;
+  }
+  std::cout << " ]" << std::endl;
+}
+
+#endif
